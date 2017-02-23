a36b274238dff9ccc2558c02ce0bddf95c980252
NJ <nathan.jayawardene@gmail.com>
2017-02-23 09:30:04 +0400

    Sign in ui custom classes complete

---
+++ coursework/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  coursework
+//
+//  Created by Nathan Jayawardene on 2/23/17.
+//  Copyright © 2017 Nathan Jayawardene. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY:CGFloat = 120.0 / 250.0
+
+
---
+++ coursework/RoundBtn.swift
@@ -0,0 +1,28 @@
+//
+//  RoundBtn.swift
+//  coursework
+//
+//  Created by Nathan Jayawardene on 2/23/17.
+//  Copyright © 2017 Nathan Jayawardene. All rights reserved.
+//
+
+import UIKit
+
+class RoundBtn: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        imageView?.contentMode = .scaleAspectFit
+
+}
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+}
+}
